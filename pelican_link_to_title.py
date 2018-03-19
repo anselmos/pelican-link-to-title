aff92d54e91099a2bc81af4dcf1dcca839258be7
Anselmos <anselmos@users.noreply.github.com>
2018-03-19 21:27:31 +0100

    Adds optimization for loopping

--- pelican_link_to_title.py
+++ pelican_link_to_title.py
@@ -7,10 +7,14 @@ import urllib
 
 def link_to_title_plugin(generator):
     "Link_to_Title plugin "
-    article_ptags = {}
+    article_ahreftag= {}
     for article in generator.articles:
         soup = BeautifulSoup(article._content, 'html.parser')
-        p_tags = soup.find_all('ahref')
+        ahref_tag = soup.find_all('ahref')
+        if ahref_tag:
+            article_ahreftag[article] = (ahref_tag, soup)
+
+    for article, (p_tags, soup) in article_ahreftag.items():
         for tag in p_tags:
             url_page = tag.string
             if url_page:
@@ -18,10 +22,12 @@ def link_to_title_plugin(generator):
                     tag.name = "a"
                     tag.string = read_page(url_page)
                     tag.attrs = {"href": url_page}
+            else:
+                continue
         article._content = str(soup).decode("utf-8")
 
-def read_page(page):
-    r = urllib.urlopen(page).read()
+def read_page(url_page):
+    r = urllib.urlopen(url_page).read()
     soup = BeautifulSoup(r , "html.parser")
     return soup.find("title").string
 
